a9acf6669d9a62c978c23de9e6a0abf5da86cba9
Matthias Koch <ithrowexceptions@gmail.com>
2019-09-18 00:42:11 +0200

    Fix parsing SDK version from global.json

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ export NUGET_XMLDOC_MODE="skip"
 ###########################################################################
 
 function FirstJsonValue {
-    perl -nle 'print $1 if m{"'$1'": "([^"\-]+)",?}' <<< ${@:2}
+    perl -nle 'print $1 if m{"'$1'": "([^"]+)",?}' <<< ${@:2}
 }
 
 # If global.json exists, load expected version
@@ -37,10 +37,13 @@ if [ -f "$DOTNET_GLOBAL_FILE" ]; then
     fi
 fi
 
+ECHO 1
 # If dotnet is installed locally, and expected version is not set or installation matches the expected version
-if [[ -x "$(command -v dotnet)" && (-z ${DOTNET_VERSION+x} || $(dotnet --version) == "$DOTNET_VERSION") ]]; then
+if [[ -x "$(command -v dotnet)" && (-z ${DOTNET_VERSION+x} || $(dotnet --version 2>&1) == "$DOTNET_VERSION") ]]; then
+    ECHO 2
     export DOTNET_EXE="$(command -v dotnet)"
 else
+    ECHO 3
     DOTNET_DIRECTORY="$TEMP_DIRECTORY/dotnet-unix"
     export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
     
